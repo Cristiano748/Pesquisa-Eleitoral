d4dadc39eeea93a45b3d1c453c1694a6e6df00d1
Cristiano748 <79268145+Cristiano748@users.noreply.github.com>
2024-09-10 11:37:43 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,13 @@
+let dados = [
+    {
+        titulo:"Aline Aguiar",
+        descricao: "Aline Aguiar Albuquerque é candidata ao cargo de Prefeito pelo PROGRESSISTAS na coligação PARA MASSAPÊ VOLTAR A SORRIR. Nascida em 08/01/1980, Aline Albuquerque Lora tem 40 anos, natural de Fortaleza - CE, Divorciado(a), Economista...",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+    titulo:"Ozires Pontes",
+    descricao: "Ozires Pontes, nascido em 1980, é herdeiro de um importante legado político em Massapê, sendo bisneto, neto e filho de deputados. Seu avô Ozires e seu pai Luiz Pontes foram os únicos massapeenses a ocupar o Senado Federal. Desde jovem, Ozires destacou-se pelo espírito empreendedor, fundando sua primeira empresa aos 18 anos e se consolidando como um empresário de sucesso no estado, gerando emprego e renda para diversas famílias.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+},
+   
+];
